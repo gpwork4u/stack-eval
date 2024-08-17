d4fdac5ec98dcbff8397bef7f7f5eb2e73d185e4
GP Wang <chunping.wang@ailabs.tw>
2024-08-17 16:41:48 +0800

    chore: adjust comment

--- crawler/common/data_model.py
+++ crawler/common/data_model.py
@@ -2,7 +2,7 @@ from dataclasses import dataclass
 from datetime import datetime
 
 
-# reference: https://www.stockfeel.com.tw/%E5%AD%B8%E7%BF%92%E5%83%B9%E5%80%BC%E6%8A%95%E8%B3%87%E4%B9%8B%E7%88%B6%E8%91%9B%E6%8B%89%E6%BC%A2%E7%9A%84%E6%8A%95%E8%B3%87%E6%96%B9%E7%A8%8B%E5%BC%8F/# https://www.ycc.idv.tw/finance/finance_10.html
+# reference: https://www.stockfeel.com.tw/%E5%AD%B8%E7%BF%92%E5%83%B9%E5%80%BC%E6%8A%95%E8%B3%87%E4%B9%8B%E7%88%B6%E8%91%9B%E6%8B%89%E6%BC%A2%E7%9A%84%E6%8A%95%E8%B3%87%E6%96%B9%E7%A8%8B%E5%BC%8F
 @dataclass
 class financial_report_10k:
     company_id: str
